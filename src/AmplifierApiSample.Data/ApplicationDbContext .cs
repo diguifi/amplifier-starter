d7c8256f037a2dc85319d4ae97c936785175f929
FabriDamazio <fabridamazio@gmail.com>
2018-10-15 22:41:46 -0300

    Add Entity Framework Core 2.1.4

---
+++ src/AmplifierApiSample.Data/ApplicationDbContext .cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AmplifierApiSample.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
